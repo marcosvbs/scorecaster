d381e390eaee8f6c54f4222870c8a51797cb82ee
Marcos Santos <rrmarcosdossantos@gmail.com>
2026-05-24 22:05:47 -0300

    feat: register Team, Match and Prediction to admin models

--- pool/admin.py
+++ pool/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Team, Match, Prediction
 
-# Register your models here.
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ["flag", "name"]
+
+    search_fields = ["name"]
+
+
+@admin.register(Match)
+class MatchAdmin(admin.ModelAdmin):
+    list_display = [
+        "home_team",
+        "away_team",
+        "phase",
+        "starts_at",
+        "home_goals",
+        "away_goals",
+    ]
+
+    list_filter = ["phase", "starts_at"]
+
+    search_fields = ["home_team__name", "away_team__name"]
+
+    readonly_fields = ["home_goals", "away_goals"]
+
+
+@admin.register(Prediction)
+class PredictionAdmin(admin.ModelAdmin):
+    list_display = ["user", "match", "home_goals", "away_goals", "result", "points"]
+
+    list_filter = ["result"]
+
+    search_fields = ["user__username"]
